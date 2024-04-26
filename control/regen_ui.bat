907e9e42e10cb5f5574989231e3718a7909d1caa
theKonon <thekonon@gmail.com>
2024-04-26 11:31:26 +0200

    Files moved to control

---
+++ control/regen_ui.bat
@@ -0,0 +1,2 @@
+pyside6-uic C:\VSCode_2\PsaciRobot\Control.ui -o C:\VSCode_2\PsaciRobot\control\my.py
+pause
--- regen_ui.bat
+++
@@ -1,2 +0,0 @@
-pyside6-uic C:\VSCode_2\PsaciRobot\Control.ui -o C:\VSCode_2\PsaciRobot\my.py
-pause
