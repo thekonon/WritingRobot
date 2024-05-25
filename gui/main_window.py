98dba24faa6f175690b4b36cd876e22ee63ab9e3
theKonon <thekonon@gmail.com>
2024-05-25 13:04:39 +0200

    Graphics merged to one file

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,58 +1,61 @@
-from PySide6.QtWidgets import QMainWindow, QPushButton, QDial, QGridLayout, QWidget
-from PySide6.QtCore import Slot
-from gui.drawer.drawer import Drawer
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QDial, QGridLayout, QWidget
+from PySide6.QtCore import Qt, Slot, Signal
 from gui.mechanics.robot import Robot
-from gui.graphics_view import GridGraphicsView
-from gui.graphics_scene import MyGraphicsScene
+from gui.graphics import MyGraphicsScene, GridGraphicsView, Drawer
 from gui.motor_dial import MotorDial
+from math import cos, sin, pi
+from typing import List
 
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
-        self.robot = Robot()
-        self.init_ui()
-        self.drawer = Drawer(self.robot, self.canvas_scene)
-        self.drawer.draw()
-        self.canvas_scene.set_updater(self.update_gui)
+        self.robot: Robot = Robot()
 
-    def init_ui(self):
         self.setWindowTitle("RobotController")
         self.setGeometry(100, 100, 600, 600)
 
-        self.canvas_scene = MyGraphicsScene(self.robot)
-        self.canvas_view = GridGraphicsView(self.canvas_scene)
+        self.canvas_scene: MyGraphicsScene = MyGraphicsScene(self.robot)
+        self.canvas_view: GridGraphicsView = GridGraphicsView(self.canvas_scene)
         self.canvas_view.setScene(self.canvas_scene)
 
-        self.connect_button = QPushButton("Connect")
+        self.connect_button: QPushButton = QPushButton("Connect")
         self.connect_button.clicked.connect(self.connect)
-        self.disconnect_button = QPushButton("Disconnect")
+        self.disconnect_button: QPushButton = QPushButton("Disconnect")
         self.disconnect_button.clicked.connect(self.disconnect)
 
-        self.motor1_dial = MotorDial()
-        self.setup_dial(self.motor1_dial, self.robot.phi[0], self.set_motor1_speed)
+        self.motor1_dial: MotorDial = MotorDial()
+        self.motor1_dial.setMinimum(0)
+        self.motor1_dial.setMaximum(360)
+        self.motor1_dial.setWrapping(True)
+        self.motor1_dial.setNotchesVisible(True)
+        self.motor1_dial.setValue(int(self.robot.phi[0] / 3.14 * 180))
+        self.motor1_dial.setInvertedAppearance(True)
+        self.motor1_dial.dialValueWhileDragging.connect(self.set_motor1_speed)
 
-        self.motor2_dial = MotorDial()
-        self.setup_dial(self.motor2_dial, self.robot.phi[1], self.set_motor2_speed)
+        self.motor2_dial: MotorDial = MotorDial()
+        self.motor2_dial.setMinimum(0)
+        self.motor2_dial.setMaximum(360)
+        self.motor2_dial.setValue(int(self.robot.phi[1] / 3.14 * 360))
+        self.motor2_dial.setWrapping(True)
+        self.motor2_dial.setNotchesVisible(True)
+        self.motor2_dial.setInvertedAppearance(True)
+        self.motor2_dial.dialValueWhileDragging.connect(self.set_motor2_speed)
 
-        self.grid_layout = QGridLayout()
+        self.grid_layout: QGridLayout = QGridLayout()
         self.grid_layout.addWidget(self.canvas_view, 0, 0, 1, 2)
         self.grid_layout.addWidget(self.connect_button, 1, 0)
         self.grid_layout.addWidget(self.disconnect_button, 1, 1)
         self.grid_layout.addWidget(self.motor1_dial, 2, 0)
         self.grid_layout.addWidget(self.motor2_dial, 2, 1)
 
-        central_widget = QWidget()
+        central_widget: QWidget = QWidget()
         central_widget.setLayout(self.grid_layout)
         self.setCentralWidget(central_widget)
 
-    def setup_dial(self, dial, phi_value, slot):
-        dial.setMinimum(0)
-        dial.setMaximum(360)
-        dial.setWrapping(True)
-        dial.setNotchesVisible(True)
-        dial.setValue(int(phi_value / 3.14 * 180))
-        dial.setInvertedAppearance(True)
-        dial.dialValueWhileDragging.connect(slot)
+        self.drawer: Drawer = Drawer(self.robot, self.canvas_scene)
+        self.drawer.draw()
+        self.canvas_scene.set_updater(self.update_gui)
 
     def update_gui(self) -> None:
         self.drawer.draw()
