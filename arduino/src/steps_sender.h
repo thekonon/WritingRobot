692509617bb5cced426be359b6ce04b56409594d
theKonon <thekonon@gmail.com>
2024-06-15 17:01:38 +0200

    Adding new module for controlling the motor

---
+++ arduino/src/steps_sender.h
@@ -0,0 +1,10 @@
+#ifndef STEPS_SENDER_H
+#define STEPS_SENDER_H
+
+void send_steps_motor_1(int steps);
+void send_steps_motor_2(int steps);
+void one_step_motor_1();
+void one_step_motor_2();
+
+
+#endif // STEPS_SENDER_H
