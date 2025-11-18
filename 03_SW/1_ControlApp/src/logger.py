6380831e05ee38694faaf4e90c8662656d1facf1
theKonon <thekonon@gmail.com>
2025-11-18 09:33:10 +0100

    move setup logger to logging

--- 03_SW/1_ControlApp/src/logger.py
+++ 03_SW/1_ControlApp/src/logger.py
@@ -1,17 +1,25 @@
 import logging
 
 
+def setup_loger() -> logging.Logger:
+    logger = logging.getLogger("RobotGUI")
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(get_logger_console_handler())
+    logger.addHandler(get_logger_file_handler())
+    return logger
+
+
 def get_logger_console_handler() -> logging.StreamHandler:
     c_handler = logging.StreamHandler()
     c_handler.setLevel(logging.INFO)
-    c_format = logging.Formatter('%(asctime)s | %(levelname)s: %(name)s: %(message)s')
+    c_format = logging.Formatter("%(asctime)s | %(levelname)s: %(name)s: %(message)s")
     c_handler.setFormatter(c_format)
     return c_handler
-    
-    
+
+
 def get_logger_file_handler() -> logging.FileHandler:
-    f_handler = logging.FileHandler('log.log')
+    f_handler = logging.FileHandler("log.log")
     f_handler.setLevel(logging.ERROR)
-    f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    f_format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     f_handler.setFormatter(f_format)
     return f_handler
